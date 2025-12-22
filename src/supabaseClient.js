880a13c3b9639c0f2c0fa23df61a09579331f02f
yoitsluisbro <cyberheavenllc@gmail.com>
2025-12-21 20:53:27 -0500

    updated supabaseClient

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,9 +1,9 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseUrl = import.meta.env.VITE_PUBLIC_SUPABASE_URL;
 const supabaseKey = 
-    import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY ||
-    import.meta.env.VITE_SUPABASE_ANON_KEY;
+    import.meta.env.VITE_PUBLIC_SUPABASE_PUBLISHABLE_KEY ||
+    import.meta.env.VITE_PUBLIC_SUPABASE_ANON_KEY;
 
 if (!supabaseUrl || !supabaseKey) {
     // This will just warn in dev if env vars are missing
